30cd2d1e8ad9d776c1daac4e091fd0f41903deb8
Koush98 <144088942+Koush98@users.noreply.github.com>
2023-12-05 16:23:46 -1200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById('mobile-menu').addEventListener('click', function() {
+  var nav = document.querySelector('.nav');
+  nav.style.display = nav.style.display === 'block' ? 'none' : 'block';
+});
